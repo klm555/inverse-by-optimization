0ff1ead5d285c3bf071075810f8c73469407522a
Woody <kristiansand555@gmail.com>
2025-04-05 11:26:38 +0800

    added forward solving func, fixed bug in axvline

--- test/local_minimum_test.py
+++ test/local_minimum_test.py
@@ -52,8 +52,8 @@ class Geometry:
         
         bound_min, bound_max = np.min(points, axis=0), np.max(points, axis=0)
         bound_diff = bound_max - bound_min
-        r_max = (min(bound_diff[0], bound_diff[1]) / 2) * 0.99 # 1% less than the boundary
-        h_max = (bound_diff[2] / 2) * 0.99
+        r_max = (min(bound_diff[0], bound_diff[1]) / 2) # 1% less than the boundary
+        h_max = (bound_diff[2] / 2)
         
         # Unnormalize the parameters
         self.cen_x = unnormalize(cen_x, bound_min[0], bound_max[0])
@@ -74,6 +74,19 @@ class Geometry:
         # Store the cells and points
         self.cells = cells
         self.points = points
+
+    def circle_forward(self) -> np.ndarray:
+        # Set the edges of the square
+        z_bot, z_top = self.cen_z - self.height, self.cen_z + self.height
+        # Find the indices of the points in the inner domain(x**2 + y**2 <= r**2) & (abs(z) <= h)            
+        domain_points = np.where(((self.points[:,0] - self.cen_x)**2 +
+                                  (self.points[:,1] - self.cen_y)**2 <= self.length ** 2) &
+                                 (self.points[:,2] >= z_bot) & (self.points[:,2] <= z_top))[0]
+        # Find the indices of the cells in the inner domain
+        domain_cells = np.any(np.isin(self.cells, domain_points), axis=1)
+        flex_inds = np.where(domain_cells)[0]
+        
+        return flex_inds # array of shape (n,)
 
     def circle(self) -> np.ndarray:
         """
@@ -191,6 +204,37 @@ class LinearElasticity(Problem):
         self.full_params = full_params
         self.internal_vars = [thetas]
 
+    # def set_params(self, params): # params = [x, y, z, r, h]
+    #     # Geometry class doesn't use 'flex_inds', but directly assigns 'theta' values to the cells
+        
+    #     # Normalize r & h
+    #     def normalize(val, vmin, vmax):
+    #         return (val - vmin) / (vmax - vmin)
+        
+    #     bound_min, bound_max = np.min(self.fes[0].points, axis=0), np.max(self.fes[0].points, axis=0)
+    #     bound_diff = bound_max - bound_min
+    #     bound_sum = bound_max + bound_min
+    #     r_max = (min(bound_diff[0], bound_diff[1]) / 2)
+    #     h_max = (bound_diff[2] / 2)
+    #     y = normalize((bound_min[1] + bound_max[1]) / 2, bound_min[1], bound_max[1])
+    #     z = normalize((bound_min[2] + bound_max[2]) / 2, bound_min[2], bound_max[2])
+    #     length = normalize(4.0, 0, r_max)
+    #     height = normalize(0.6, 0, h_max)
+
+    #     inner_domain = Geometry(params[0], y, z, length, height, 
+    #                             self.fes[0].cells, self.fes[0].points)
+    #     flex_inds = inner_domain.circle_forward()
+        
+    #     # Create an array of size (num_cells, 1)
+    #     full_params = np.ones((problem.num_cells, 1))
+    #     # Assign the elastic modulus "E_in" to the cells with flex_inds
+    #     full_params = full_params.at[flex_inds].set(0.)
+    #     full_params = np.expand_dims(full_params, axis=1)
+    #     thetas = np.repeat(full_params[:, None, :], self.fes[0].num_quads, axis=1)
+    #     self.params = params
+    #     self.full_params = full_params
+    #     self.internal_vars = [thetas]
+
     # Traction
     def get_surface_maps(self):
         def surface_map(u, x):
@@ -275,29 +319,31 @@ outputs = []
 for x in x_points:
     rho = np.array([x])
     rho_normalized = normalize(rho, bound_min[0], bound_max[0])
-    
     output = J_total(rho_normalized)
     outputs.append(output)
     print(f"Iteration:{count}")
     count += 1
     
-x_center_normalized = np.array([0.5])
-obj_0 = J_total(x_center_normalized)
+x_center = np.array([(bound_min[0] + bound_max[0]) / 2])
+obj_0 = J_total(normalize(x_center, bound_min[0], bound_max[0]))
 
 # Plot the optimization results.
 obj = onp.array(outputs)
 plt.figure(1, figsize=(10, 8))
-plt.plot(onp.arange(len(obj)) + 1, obj, linestyle='-', linewidth=2, color='black')
+plt.plot(x_points, obj, linestyle='-', linewidth=2, color='black')
 plt.axhline(y=obj_0, color='r', linestyle='--', label='J = %f' % obj_0)
-plt.xlabel(r"x coordinate", fontsize=20)
+plt.axvline(x=x_center, color='r', linestyle='--', label='x = %f' % x_center[0])
+plt.xlabel(r"x-coordinate", fontsize=20)
 plt.ylabel(r"Objective value", fontsize=20)
+plt.ylim(0, 1.5)
 plt.legend(fontsize=20)
 plt.tick_params(labelsize=20)
 plt.tick_params(labelsize=20)
-plt.title('Objective function w.r.t x coordinate', fontsize=20)
+plt.title('Objective function w.r.t x-coordinate', fontsize=20)
 # plt.title(rf'Initial Guess with $\theta = {vf}$', fontsize=20)
 
 save_dir = 'data/inverse/figures'
 os.makedirs(save_dir, exist_ok=True)  # Create directory if it doesn't exist
 plt.savefig('data/inverse/figures/local_min_test.png', dpi=300, bbox_inches='tight')
 # plt.show()
+onp.savetxt('data/inverse/figures/local_min_test.txt', obj)
