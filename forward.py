58a9f1e35f11c2c38ac14930310dc246e80f286b
Woody <kristiansand555@gmail.com>
2025-04-12 11:09:40 +0800

    cminor bug fixed

--- forward.py
+++ forward.py
@@ -13,7 +13,7 @@ import meshio
 import logging
 logger.setLevel(logging.DEBUG)
 
-file_name = 'u_dogbone_0.01_any_both_fixed'
+file_name = 'u_dogbone_0.01'
 
 # Inner domain (soft material)
 E_inner = 1.0e-3
@@ -137,7 +137,7 @@ class Geometry:
                                   (self.points[:,1] - self.center[1])**2 <= self.length ** 2) &
                                  (self.points[:,2] >= z_bot) & (self.points[:,2] <= z_top))[0]
         # Find the indices of the cells in the inner domain
-        domain_cells = np.all(np.isin(self.cells, domain_points), axis=1)
+        domain_cells = np.any(np.isin(self.cells, domain_points), axis=1)
         flex_inds = np.where(domain_cells)[0]
         
         return flex_inds # array of shape (n,)
